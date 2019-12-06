c1223aa923c7d10d9bbd64cb44a8677f0a3f8a10
Rob Bavey <rob.bavey@elastic.co>
2019-12-06 17:09:19 -0500

    Switch to using manticore (#111)
    * Switch to using manticore
    
    * Add Secure integration tests
    
    * Changelog and version bump
    Remove beta qualifier from gemspec
    
    * Minor tweak to helper code
    
    * Pin Faraday to 0.15 to avoid breaking change
    0.17 contains code that breaks with Logstash 5.x
    
    * Update lib/logstash/inputs/elasticsearch.rb
    Co-Authored-By: Colin Surprenant <colin.surprenant@gmail.com>

--- ci/logstash-run.sh
+++ ci/logstash-run.sh
@@ -6,6 +6,9 @@ export PATH=$BUILD_DIR/gradle/bin:$PATH
 wait_for_es() {
   echo "Waiting for elasticsearch to respond..."
   es_url="http://elasticsearch:9200"
+  if [[ "$SECURE_INTEGRATION" == "true" ]]; then
+    es_url="https://elasticsearch:9200 -k"
+  fi
   count=120
   while ! curl --silent $es_url && [[ $count -ne 0 ]]; do
     count=$(( $count - 1 ))
@@ -18,9 +21,13 @@ wait_for_es() {
 }
 
 if [[ "$INTEGRATION" != "true" ]]; then
-  bundle exec rspec -fd spec/inputs -t ~integration
+  bundle exec rspec -fd spec/inputs -t ~integration -t ~secure_integration
 else
-  extra_tag_args="--tag integration"
+  if [[ "$SECURE_INTEGRATION" == "true" ]]; then
+    extra_tag_args="--tag secure_integration"
+  else
+    extra_tag_args="--tag ~secure_integration --tag integration"
+  fi
   wait_for_es
   bundle exec rspec -fd $extra_tag_args --tag es_version:$ELASTIC_STACK_VERSION spec/inputs/integration
 fi
