dffb67ecc0c19fd756ed39e65ebf905747f9671b
Edmo Vamerlatti Costa <11836452+edmocosta@users.noreply.github.com>
2023-03-06 21:28:30 +0100

    Update docker-setup.sh

--- .ci/docker-setup.sh
+++ .ci/docker-setup.sh
@@ -4,43 +4,15 @@
 # Ensure you have Docker installed locally and set the ELASTIC_STACK_VERSION environment variable.
 set -e
 
-
-download_and_load_docker_snapshot_artifact() {
-  project="${1?project name required}"
-
-  artifact_type="docker-image"
-  artifact_name_base="${project}${DISTRIBUTION_SUFFIX}-${ELASTIC_STACK_VERSION}-${artifact_type}"
-  echo "Downloading snapshot docker image: ${project}${DISTRIBUTION_SUFFIX} (${ELASTIC_STACK_VERSION})"
-
-  artifact_name_noarch="${artifact_name_base}.tar.gz"
-  artifact_name_arch="${artifact_name_base}-x86_64.tar.gz"
-
-  jq_extract_artifact_url=".build.projects.\"${project}\".packages | (.\"${artifact_name_noarch}\" // .\"${artifact_name_arch}\") | .url"
-
-  artifact_list=$(curl --silent "https://artifacts-api.elastic.co/v1/versions/${ELASTIC_STACK_VERSION}/builds/latest")
-  artifact_url=$(echo "${artifact_list}" | jq --raw-output "${jq_extract_artifact_url}")
-
-  if [[ "${artifact_url}" == "null" ]]; then
-    echo "Failed to find '${artifact_name_noarch}'"
-    echo "Failed to find '${artifact_name_arch}'"
-    echo "Listing:"
-    echo "${artifact_list}" | jq --raw-output ".build.projects.\"${project}\".packages | keys | map(select(contains(\"${artifact_type}\")))"
-    return 1
-  fi
-
-  echo "${artifact_url}"
-
-  cd /tmp
-  curl "${artifact_url}" > "${project}-docker-image.tar.gz"
-  tar xfvz "${project}-docker-image.tar.gz" repositories
-  echo "Loading ${project} docker image: "
-  cat repositories
-  docker load < "${project}-docker-image.tar.gz"
-  rm "${project}-docker-image.tar.gz"
-  cd -
-}
-
 VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/master/ci/logstash_releases.json"
+
+
+pull_docker_snapshot() {
+  project="${1?project name required}"
+  local docker_image="docker.elastic.co/${project}/${project}:${ELASTIC_STACK_VERSION}"
+  echo "Pulling $docker_image"
+  docker pull "$docker_image"
+}
 
 if [ "$ELASTIC_STACK_VERSION" ]; then
     echo "Fetching versions from $VERSION_URL"
@@ -62,9 +34,9 @@ if [ "$ELASTIC_STACK_VERSION" ]; then
     echo "Testing against version: $ELASTIC_STACK_VERSION"
 
     if [[ "$ELASTIC_STACK_VERSION" = *"-SNAPSHOT" ]]; then
-        download_and_load_docker_snapshot_artifact "logstash"
+        pull_docker_snapshot "logstash"
         if [ "$INTEGRATION" == "true" ]; then
-            download_and_load_docker_snapshot_artifact "elasticsearch"
+          pull_docker_snapshot "elasticsearch"
         fi
     fi
 
@@ -86,4 +58,3 @@ else
     echo "For example: export ELASTIC_STACK_VERSION=6.2.4"
     exit 1
 fi
-
