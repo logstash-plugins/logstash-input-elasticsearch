f96dad3c80eb27dfd9a76fc5bb555946a7375f2a
Edmo Vamerlatti Costa <11836452+edmocosta@users.noreply.github.com>
2023-06-02 18:01:09 +0200

    fix: restore use of DefaultHostnameVerifier (#193)
    * Document why we are using :default instead of :strict
    
    Co-authored-by: Ry Biesemeyer <yaauie@users.noreply.github.com>

--- lib/logstash/inputs/elasticsearch.rb
+++ lib/logstash/inputs/elasticsearch.rb
@@ -521,7 +521,9 @@ class LogStash::Inputs::Elasticsearch < LogStash::Inputs::Base
                         "to make sure your data is secure set `ssl_verification_mode => full`"
           ssl_options[:verify] = :disable
         else
-          ssl_options[:verify] = :strict
+          # Manticore's :default maps to Apache HTTP Client's DefaultHostnameVerifier,
+          # which is the modern STRICT verifier that replaces the deprecated StrictHostnameVerifier
+          ssl_options[:verify] = :default
       end
     end
 
