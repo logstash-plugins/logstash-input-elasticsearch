a0a88f5c3ab88959e8c468b3c4441e0b589a1157
João Duarte <jsvduarte@gmail.com>
2024-01-22 13:57:01 +0000

    make agg retryable

--- lib/logstash/inputs/elasticsearch/aggregation.rb
+++ lib/logstash/inputs/elasticsearch/aggregation.rb
@@ -6,26 +6,38 @@ module LogStash
       class Aggregation
         include LogStash::Util::Loggable
 
+        AGGREGATION_JOB = "aggregation"
+
         def initialize(client, plugin)
           @client = client
           @plugin_params = plugin.params
 
-          @scroll = @plugin_params["scroll"]
           @size = @plugin_params["size"]
-          @index = @plugin_params["index"]
           @query = @plugin_params["query"]
+          @retries = @plugin_params["retries"]
           @agg_options = {
             :index => @index,
-            :size  => @size
+            :size  => 0
           }.merge(:body => @query)
 
           @plugin = plugin
         end
 
+        def retryable(job_name, &block)
+          stud_try = ::LogStash::Helpers::LoggableTry.new(logger, job_name)
+          stud_try.try((@retries + 1).times) { yield }
+        rescue => e
+          error_details = {:message => e.message, :cause => e.cause}
+          error_details[:backtrace] = e.backtrace if logger.debug?
+          logger.error("Tried #{job_name} unsuccessfully", error_details)
+        end
+
         def do_run(output_queue)
           logger.info("Aggregation starting")
-          r = @client.search(@agg_options)
-          @plugin.push_hit r, output_queue, 'aggregations'
+          retryable(AGGREGATION_JOB) do
+            r = @client.search(@agg_options)
+            @plugin.push_hit(r, output_queue, 'aggregations')
+          end
         end
       end
     end
