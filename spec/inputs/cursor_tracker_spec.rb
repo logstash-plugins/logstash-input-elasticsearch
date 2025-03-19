b3d81c992cfd76a0b2112c39eb56e8451b1acc5c
João Duarte <jsvduarte@gmail.com>
2025-03-19 16:48:55 +0000

    minor test tweaks

--- spec/inputs/cursor_tracker_spec.rb
+++ spec/inputs/cursor_tracker_spec.rb
@@ -24,6 +24,7 @@ describe LogStash::Inputs::Elasticsearch::CursorTracker do
 
   describe "checkpoint_cursor" do
     before(:each) do
+      subject.checkpoint_cursor(intermediate: false) # store seed value
       [
         Thread.new(subject) {|subject| subject.record_last_value(LogStash::Event.new("my_field" => "2025-01-03T23:59:59.999999999Z")) },
         Thread.new(subject) {|subject| subject.record_last_value(LogStash::Event.new("my_field" => "2025-01-01T23:59:59.999999999Z")) },
@@ -31,14 +32,14 @@ describe LogStash::Inputs::Elasticsearch::CursorTracker do
       ].each(&:join)
     end
     context "when doing intermediate checkpoint" do
-      before(:each) { subject.checkpoint_cursor(intermediate: true) }
       it "persists the smallest value" do
+        subject.checkpoint_cursor(intermediate: true)
         expect(IO.read(last_run_metadata_path)).to eq("2025-01-01T23:59:59.999999999Z")
       end
     end
     context "when doing non-intermediate checkpoint" do
-      before(:each) { subject.checkpoint_cursor(intermediate: false) }
       it "persists the largest value" do
+        subject.checkpoint_cursor(intermediate: false)
         expect(IO.read(last_run_metadata_path)).to eq("2025-01-03T23:59:59.999999999Z")
       end
     end
