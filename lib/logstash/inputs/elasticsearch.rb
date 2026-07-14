0ad4f46eddc77182623d6e0cdc62281202b0f6bf
Álex Cámara <alex.camara@elastic.co>
2026-07-14 14:34:43 +0200

    Remove Rufus scheduler

--- lib/logstash/inputs/elasticsearch.rb
+++ lib/logstash/inputs/elasticsearch.rb
@@ -312,8 +312,6 @@ class LogStash::Inputs::Elasticsearch < LogStash::Inputs::Base
   end
 
   def register
-    require "rufus/scheduler"
-
     @pipeline_id = execution_context&.pipeline_id || 'main'
 
     fill_hosts_from_cloud_id
